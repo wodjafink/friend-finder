b479f2648e2dc5368efef70a1232472869a0d5d8
wodjafink <wodjafink@gmail.com>
2018-05-19 15:24:48 -0700

    Now have route being served for home page

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,6 @@ var friends = require("../data/friends.js");
 
 module.exports = function(app) {
 
-	console.log(friends)
 
 	app.get("/api/friends", function(req, res){
 		res.json(friends)
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,7 +1,11 @@
-var friendsData = require("../data/friends");
+var path = require("path")
 
 module.exports = function(app) {
-	// app.get("/api/friends", function(req, res){
-	// 	res.json(friendsData)
-	// })
+	app.get("/survey", function(req, res){
+    res.sendFile(path.join(__dirname, "../public/survey.html"));
+	})
+
+  app.get("*", function(req, res) {
+    res.sendFile(path.join(__dirname, "../public/home.html"));
+  });
 }
